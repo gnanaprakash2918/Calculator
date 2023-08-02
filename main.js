6fe1a064df98616defb5a04b642bb20bfdbdc57d
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-02 18:34:45 +0530

    Removed output top functionality

--- main.js
+++ main.js
@@ -1,55 +1,9 @@
 // Operators List
 const operators = ["+", "-", "/", "X", "=", "%"];
 
-function precedence(operator) {
-  switch (operator) {
-    case "+":
-    case "-":
-      return 1;
-
-    case "*":
-    case "/":
-      return 2;
-
-    default:
-      return -1;
-  }
+function evaluateExpression(expr) {
+  return eval(expr);
 }
-
-//Convert Infix to PostFix Expression
-function InfixToPostFix(expr) {
-  let stack = [];
-  let result = "";
-
-  for (let i = 0; i < expr.length; i++) {
-    let c = expr[i];
-
-    if (
-      (c >= "a" && c <= "z") ||
-      (c >= "A" && c <= "Z") ||
-      (c >= "0" && c <= "9")
-    )
-      result += c;
-    else {
-      while (
-        !(stack.length == 0) &&
-        precedence(expr[i]) <= precedence(stack[stack.length - 1])
-      ) {
-        result += stack.pop();
-      }
-      stack.push(c);
-    }
-  }
-
-  while (!stack.length == 0) {
-    result += stack.pop();
-  }
-
-  return result;
-}
-
-// Evaluate Expression Function
-function evaluateExpression(expr) {}
 
 //DOM elements
 const numberButtons = Array.from(document.querySelectorAll(".number-btn"));
@@ -64,8 +18,6 @@ let numberClicked = false;
 //Add eventListeners to numberButtons
 numberButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    if (operators.includes(prevNum.charAt(prevNum.length - 2)))
-      outputBottom.textContent = "";
     const txt = e.target.textContent;
     outputBottom.textContent += txt;
     prevNum += txt;
@@ -75,11 +27,8 @@ numberButtons.forEach((btn) => {
 //Add eventListeners to operatorButtons
 operatorButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    // outputBottom.textContent = evaluateExpression(prevNum);
     const currOperator = e.target.textContent;
-    outputTop.textContent += `${outputBottom.textContent} ${currOperator} `;
-    prevNum += `${outputBottom.textContent} ${currOperator} `;
+    outputBottom.textContent += ` ${currOperator} `;
+    prevNum += ` ${currOperator} `;
   });
 });
-
-console.log(InfixToPostFix("a+b*c^d-e^f+g*h-i"));
