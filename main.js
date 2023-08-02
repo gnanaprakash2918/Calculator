35385a11327a457bb1dcdf67baa5b764c08a4392
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-02 08:59:53 +0530

    Add separate event-listeners to numbers and operators

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ function evaluateExpression(expr) {}
 
 const operators = ["+", "-", "/", "X", "=", "%"];
 
-const buttons = Array.from(document.querySelectorAll(".number-btn,.operator"));
+const numberButtons = Array.from(document.querySelectorAll(".number-btn"));
+const operatorButtons = Array.from(document.querySelectorAll(".operator"));
 const outputTop = document.querySelector(".output-top");
 const outputBottom = document.querySelector(".output-bottom");
 
 const history = "";
+let prevNum = "";
 
-buttons.forEach((btn) => {
+numberButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const txt = e.target.textContent;
-    if (operators.includes(txt)) {
-      outputTop.textContent += `${outputBottom.textContent} ${txt} `;
-    } else {
-      outputBottom.textContent += txt;
-    }
+    outputBottom.textContent += txt;
+    prevNum += txt;
   });
+});
+
+operatorButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {});
 });
