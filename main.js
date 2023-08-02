849802fdf88cdd40daeb1a9abd3c3b68a9f17c6d
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-02 07:43:24 +0530

    Update font-size in output-bottom

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+const operators = "+/*-=";
+
 const buttons = Array.from(document.querySelectorAll(".input-btn"));
 const output = document.querySelector(".output-bottom");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
+    const txt = e.target.textContent;
     output.textContent += e.target.textContent;
   });
 });
