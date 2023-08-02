3c08e13af364607f00f6cef057681ec53836ee1c
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-08-02 07:51:33 +0530

    Add space to output-screen after operator

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ const output = document.querySelector(".output-bottom");
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const txt = e.target.textContent;
-    output.textContent += e.target.textContent;
+    output.textContent += operators.find(txt) != -1 ? txt : ` ${txt} `;
   });
 });
